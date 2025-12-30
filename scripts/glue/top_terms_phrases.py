3522d4ee9e89c3ad8d30355b5f5780ab49780e6d
Charles Adeiye <adeiyecharles@gmail.com>
2025-12-30 18:36:29 +0000

    datetime error fix

--- scripts/glue/top_terms_phrases.py
+++ scripts/glue/top_terms_phrases.py
@@ -1,5 +1,5 @@
 import sys
-from datetime import datetime
+import datetime
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, lower, regexp_replace, split, explode, lit, count, desc, concat_ws, when
 from pyspark.sql.window import Window
